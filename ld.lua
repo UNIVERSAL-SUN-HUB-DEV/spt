04fdd4619001506c3d522641a9b622439550f909
UNIVERSAL-SUN-HUB-DEV <292715108+UNIVERSAL-SUN-HUB-DEV@users.noreply.github.com>
2026-09-24 20:00:38 +0530

    Update ld.lua

--- ld.lua
+++ ld.lua
@@ -1,7 +1,15 @@
-local success, result = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/sanjeevthakur154a-jpg/nehhinnjfiueioerok/refs/heads/main/k2.lua?t=" .. tick()))()
-end)
+local success, result = pcall(function() 
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/sanjeevthakur154a-jpg/nehhinnjfiueioerok/refs/heads/main/KEYGATE.lua?t=" .. tick()))() 
+end) 
 
-if not success then
-    warn("Failed to load script: " .. tostring(result))
+if success then
+    pcall(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Key NOTICE!",
+            Text = "The script may take up to 30 seconds to load.",
+            Duration = 7
+        })
+    end)
+else 
+    warn("Failed to load script: " .. tostring(result)) 
 end
